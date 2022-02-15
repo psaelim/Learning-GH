7dc901fe81aa038742f475b12abd2d5c0f30d801
June Saelim <june169@gmail.com>
2022-02-15 16:26:09 -0500

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by June Saelim on 2/15/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
